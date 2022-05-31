8a524b47603b9702329b9fdd8b42b35dc2f42beb
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-05-31 19:36:57 +0530

    Head and Image from nextjs

--- next-api/pages/_app.js
+++ next-api/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Yash raj</title>
+      </Head>
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
